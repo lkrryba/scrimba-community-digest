b94a3d06065037f7fcf0ae67443a9e844f231737
Leanne <leanne.rybintsev@gmail.com>
2021-11-15 14:37:44 +0000

    nav bar and styling

---
+++ components/nav.js
@@ -0,0 +1,48 @@
+//get end of url to decide whether to display "home" on menu
+const url = window.location.href
+const lastSegment = url.split('/').pop()
+
+// Mobile Nav Opener only loads after html has parsed else "navOpener" doesn't exist
+document.addEventListener('DOMContentLoaded', (event) => {
+  const navOpener = document.querySelector('.nav-opener')
+  const nav = document.querySelector('nav')
+
+  navOpener.addEventListener('click', () => {
+    nav.classList.toggle('nav-open')
+    navOpener.classList.toggle('exit')
+  })
+})
+
+// the nav html
+class Nav extends HTMLElement {
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <!-- Start of Nav -->
+    <div class="nav-opener">
+        <div class="nav-icon-line-1"></div>
+        <div class="nav-icon-line-2"></div>
+        <div class="nav-icon-line-3"></div>
+    </div>
+    <nav>
+      <a class="nav-scrimba" href="https://scrimba.com/" rel="noopener" target="_blank"><img class="nav-logo" src="../../../imgs/scrimba-black.svg" alt="scrimba-logo"></a>
+      <div class="nav-other-links">
+        ${
+          lastSegment.length !== 0
+            ? `<a href="/" rel="noopener" target="_blank">Home</a>`
+            : ``
+        } 
+        <a target="_blank" rel="noopener" href="https://scrimba.com/learn/weeklychallenge/the-weekly-web-dev-challenge-dark-light-theme-latest-challenge-code-to-win-co24f4cd08039faab7eff7a98">Current Challenge</a>  
+        <a href="/hall-of-fame.html" rel="noopener" target="_blank">Hall of Fame</a>
+        <a href="/#prev_challenges">Previous Challenges</a>
+        <a href="/faq.html" rel="noopener" target="_blank">FAQ</a>
+      </div>
+    </nav>    
+    `
+  }
+}
+
+customElements.define('nav-component', Nav)
